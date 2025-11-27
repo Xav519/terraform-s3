82bc2d6cecb53783175792e457df54a74eeea0c6
Xav519 <dupuis_x@yahoo.com>
2025-11-26 23:06:35 -0500

    Initial commit for s3 bucket creation with terraform

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "xav-terraform-bucket-2025"
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
